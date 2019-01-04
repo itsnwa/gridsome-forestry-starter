d8e14a1d9a053834f0620d18bfdc89c2ce41370b
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-04 19:11:46 +0100

    added logo etc to the index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <Layout>
+    <img :src="settings.logo" :alt="settings.site_name" />
     <h1>Projects</h1>
     <div v-for="item in $page.posts.edges" :key="item.node.id">
       <h1>{{ item.node.title }}</h1>
@@ -9,6 +10,7 @@
         Read more
       </g-link>
     </div>
+    <p>{{ settings.footer }}</p>
   </Layout>
 </template>
 
@@ -31,5 +33,10 @@ query Posts {
 
 <script>
 export default {
+  data() {
+    return {
+      settings: require("../../data/theme.json")
+    }
+  }
 }
 </script>
