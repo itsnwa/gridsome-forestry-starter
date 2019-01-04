8ad06d216614181ebef69e383eabab08262ffcc9
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-04 22:52:18 +0100

    Defined grid for project view

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,14 +4,14 @@
       <Hero />
       <div class="projects">
         <div v-for="item in $page.posts.edges" :key="item.node.id" class="project">
-          <h2>{{ item.node.title }}</h2>
+          <!-- <h2>{{ item.node.title }}</h2> -->
           <g-image :src="item.node.thumbnail" :alt="item.node.title + 'thumbnail'" />
-          <div class="categories">
+          <!-- <div class="categories">
             <span class="category" v-for="(item, index) in item.node.categories" :key="index">{{ item }}</span>
-          </div>
-          <g-link :to="item.node.path">
+          </div> -->
+          <!-- <g-link :to="item.node.path">
             Read more
-          </g-link>
+          </g-link> -->
         </div>
       </div>
     </div>
@@ -49,3 +49,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.projects {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 4rem;
+}
+.project {
+  grid-column: auto / span 1;
+}
+.project:nth-child(3n) {
+  grid-column: auto / span 2;
+}
+</style>
