84eee0f3a7794aee441f51ed969f73a16545bf6c
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-05 14:28:30 +0100

    Better component structure

---
+++ src/components/LatestJournals.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="latest-journals-heading container">
+        <h3>Latest and greatest</h3>
+        </div>
+        <div class="latest-journals">
+        <div class="container">
+            <g-link :to="item.node.path" class="journal" v-for="item in journals" :key="item.node.id">
+            <h3 class="journal-title">{{ item.node.title }}</h3>
+            </g-link>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        journals: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.latest-journals-heading {
+  margin-top: 6rem;
+  margin-bottom: 1rem;
+  font-size: 0.6rem;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+.latest-journals {
+  border-top: 1px solid #f3f3f3;
+  border-bottom: 1px solid #f3f3f3;
+}
+.latest-journals > .container {
+  display: flex;
+}
+.journal {
+  flex: 1;
+  display: block;
+  padding: 2rem;
+  border-right: 1px solid #f3f3f3;
+  transition: background 0.25s ease;
+  text-decoration: none;
+}
+.journal:last-of-type {
+  border: 0;
+}
+.journal:hover {
+  background: #f7f7f7;
+}
+</style>
---
+++ src/components/ProjectsGrid.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="projects">
+        <div class="project" v-for="item in projects" :key="item.node.id">
+            <g-link :to="item.node.path" class="project-link">
+            <g-image
+                :src="item.node.thumbnail"
+                :alt="item.node.title"
+                class="thumbnail"
+            />
+            <h3 class="project-title">{{ item.node.title }}</h3>
+            <div class="categories">
+                <span class="category" v-for="(item, index) in item.node.categories" :key="index">{{ item }}</span>
+            </div>
+            </g-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        projects: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.projects {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 4rem;
+}
+.project {
+  grid-column: auto / span 1;
+  text-align: center;
+}
+.project:nth-child(3n+1) {
+  grid-column: auto / span 2;
+}
+.project-link {
+  text-decoration: none;
+}
+.thumbnail {
+  height: 560px;
+  object-fit: cover;
+}
+.project-title {
+  font-size: 1rem;
+  margin: 2rem 0 1rem 0;
+}
+.categories {
+  font-size: 0.8rem;
+  opacity: 0.6;
+}
+.category {
+  margin-right: 0.8rem;
+}
+.category:last-of-type {
+  margin: 0;
+}
+</style>
