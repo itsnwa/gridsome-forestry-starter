a1b38ac1780c9fe7754859ec2b09fff27d82057b
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-06 22:07:59 +0100

    added contact form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
             </div>
             <nav class="nav right">
                 <g-link class="nav__link" :to="{ name: 'journal' }">Journal</g-link>
-                <g-link class="nav__link" :to="{ name: 'about' }">Say Hi!</g-link>
+                <g-link class="nav__link" :to="{ name: 'contact' }">Say Hi!</g-link>
             </nav>
         </div>
     </header>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,99 @@
+<template>
+  <Layout>
+    <div class="container">
+
+      <div class="contact-header">
+        <h1 class="contact-title">Say hi!</h1>
+        <p>Leave me a note with any questions you might have, I'll get back to you as soon as possible.</p>
+      </div>
+
+      <form class="contact-form" name="contact" method="POST" data-netlify="true">
+
+        <div class="sender-info">
+          <div>
+            <label for="name" class="label">Your name</label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label for="email" class="label">Your email</label>
+            <input type="email" name="email" />
+          </div>
+        </div>
+
+        <div class="message">
+          <label for="message" class="label">Message</label>
+          <textarea name="message"></textarea>
+        </div>
+
+        <button type="submit" class="button">Submit form</button>
+
+      </form>
+
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.contact-header {
+  padding: 2rem 0 4rem 0;
+}
+.contact-title {
+  font-size: 4rem;
+  margin: 0 0 4rem 0;
+  padding: 0;
+}
+.sender-info {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 2rem;
+}
+.sender-info > div {
+  flex: 1;
+  margin-right: 4rem;
+}
+.sender-info > div:last-of-type {
+  margin: 0;
+}
+input:focus,textarea:focus {
+  border-color: var(--color-contrast-1);
+}
+input,textarea {
+  background: transparent;
+  border: 1px solid var(--color-base-1);
+  outline: none;
+  border-radius: 0.3rem;
+  padding: 0.8rem 1rem;
+  color: inherit;
+  font-size: 1rem;
+  width: 100%;
+}
+textarea {
+  resize: none;
+  height: 140px;
+}
+.button {
+  color: var(--color-base);
+  background: var(--color-contrast);
+  outline: none;
+  border: 0;
+  font-size: 0.8rem;
+  padding: 0.8rem 1.6rem;
+  border-radius: 0.3rem;
+  margin-top: 2rem;
+  cursor: pointer;
+  transition: opacity 0.25s ease;
+  font-size: 500;
+  letter-spacing: 0.035em;
+}
+.button:hover {
+  opacity: 0.6;
+}
+.button:focus {
+  border: 1px solid var(--color-base-1);
+}
+</style>
+
