aae07da3e895ed287efbaec886538c00aa86cfe7
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-06 20:38:32 +0100

    Started journal styling

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -1,13 +1,33 @@
 <template>
   <Layout>
     <div class="journal">
-        <g-link to="/journal">
-          Back
-        </g-link>
-        <h1 v-html="$page.post.title"/>
-        <div v-html="$page.post.date"/>
-        <p>{{ $page.post.timeToRead }} min read</p>
+
+      <div class="journal-header">
+        <div class="container">
+          <h1 v-html="$page.post.title" class="journal-title" />
+          <div class="journal-meta">
+            <div class="journal-author">
+              <span class="label">Author</span>
+              <div class="author-avatar">
+                Author avatar
+              </div>
+            </div>
+            <div class="journal-date">
+              <span class="label">Date</span>
+              <div v-html="$page.post.date"/>
+            </div>
+            <div class="journal-time">
+              <span class="label">Time</span>
+              <span>{{ $page.post.timeToRead }} min read</span>
+            </div>
+          </div>          
+        </div>
+      </div>
+
+      <div class="container">
         <div v-html="$page.post.content"/>
+      </div>
+
     </div>
   </Layout>
 </template>
@@ -34,6 +54,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.journal-header {
+  padding: 2rem 0 4rem 0;
+}
+.journal-title {
+  font-size: 4rem;
+  margin: 0 0 4rem 0;
+  padding: 0;
+}
+.journal-meta {
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 0.8rem;
+}
+.journal-meta > div {
+  margin-right: 4rem;
+}
+.journal-meta > div:last-of-type {
+  margin: 0;
+}
 </style>
