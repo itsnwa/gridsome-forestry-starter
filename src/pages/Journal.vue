88f38423f50d2ceb01a2ddc5406552e88df130a9
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-06 14:20:28 +0100

    added simple container

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -1,14 +1,15 @@
 <template>
   <Layout>
-    <h1>Journal</h1>
-    <div v-for="item in $page.posts.edges" :key="item.node.id">
-      <h1>{{ item.node.title }}</h1>
-      <p>{{ item.node.date }}</p>
-      <p>{{ item.node.excerpt }}</p>
-      <g-link :to="item.node.path">
-        Read more
-      </g-link>
-    </div> 
+    <div class="container">
+      <div v-for="item in $page.posts.edges" :key="item.node.id">
+        <h1>{{ item.node.title }}</h1>
+        <p>{{ item.node.date }}</p>
+        <p>{{ item.node.excerpt }}</p>
+        <g-link :to="item.node.path">
+          Read more
+        </g-link>
+      </div>
+    </div>
   </Layout>
 </template>
 
@@ -32,3 +33,9 @@ query Journal {
 export default {
 }
 </script>
+
+<style scoped>
+  .container {
+    padding-top: 6rem;
+  }
+</style>
