f331d131a9211f91393f5417de4775aaf7c64e3d
Nichlas W. Andersen <nichlas@itsnwa.com>
2019-01-07 00:00:27 +0100

    added code highlighting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import settings from "../data/theme.json";
 
+import "@/assets/code-highlight.css"
+
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
