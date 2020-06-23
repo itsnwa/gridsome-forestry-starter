6e765700499d1fbef17730e30c8a519a9ad66890
Carter Snook <cartersnook04@gmail.com>
2020-06-22 23:45:48 -0500

    Better date format?

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -33,7 +33,7 @@ query JournalPost ($path: String!) {
   post: journalPost (path: $path) {
     title
     author
-    date (format: "D. MMMM YYYY")
+    date (format: "MMMM D, YYYY")
     timeToRead
     content
   }
